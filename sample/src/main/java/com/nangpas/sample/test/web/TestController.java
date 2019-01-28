cb8ec11635fd4de80d4e8eac88852f57f7de62ed
nangpas <nangpas@comPC>
2019-01-28 14:43:25 +0900

    프로젝트 등록 (이재원)
    수정자 : 이재원
    수정일자 : 2019-01-28
    수정내용
     - 프로젝트 등록

---
+++ sample/src/main/java/com/nangpas/sample/test/web/TestController.java
@@ -0,0 +1,22 @@
+package com.nangpas.sample.test.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@RequestMapping("/test")
+	public String test(Model model) {
+		logger.debug("logger test debug");
+		logger.info("logger test info");
+		logger.error("logger test error");
+		
+		model.addAttribute("name", "test");
+		return "test";
+	}
+}
