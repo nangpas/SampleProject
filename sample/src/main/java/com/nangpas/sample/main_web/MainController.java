8805725d191b7c99861fbabb29d445f3958d39e0
fsadmin <fsadmin@DESKTOP-QFT42E5>
2019-01-28 17:30:00 +0900

    jsp,controller 추가
    수정자:박일
    수정일자 2019-01-28
    수정내용
    - 파일 추가 (내용x)

---
+++ sample/src/main/java/com/nangpas/sample/main_web/MainController.java
@@ -0,0 +1,21 @@
+package com.nangpas.sample.main_web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	
+	@RequestMapping("/main")
+	public String test(Model model) {
+
+		
+		
+		model.addAttribute("name", "test");
+		return "test";
+	}
+}
